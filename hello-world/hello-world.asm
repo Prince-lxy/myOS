2c96a44db6afa3038178399b7f45586a7ef92efb
Prince-lxy <946875720@qq.com>
2019-07-16 15:29:24 +0800

    跳转 hello-world.asm 格式

--- hello-world/hello-world.asm
+++ hello-world/hello-world.asm
@@ -1,6 +1,6 @@
-start:
-	org 0x7c00
+org 0x7c00
 
+start:
 	;; es:bp = 字符串首地址
 	mov ax, cx
 	mov es, ax
@@ -9,7 +9,7 @@ start:
 
 	mov ax, 0x1301		;; ah = 功能代码 al = 写模式
 	mov bx, 0x000c		;; bh = 页码 bl = 颜色
-	mov cx, msgLen	;; cx = 字符串长度
+	mov cx, msgLen		;; cx = 字符串长度
 	mov dx, 0x0000		;; dh = 行 dl = 列
 	int 0x10
 	jmp $
