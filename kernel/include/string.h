fedaaa7f33ebfabd60a43f93d0921564fd290445
Prince-lxy <946875720@qq.com>
2019-08-20 11:03:51 +0800

    [kernel] 添加 kernel 打印字符串函数

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -1,1 +1,2 @@
 PUBLIC	void* memcpy(void* p_dst, void* p_src, int size);
+PUBLIC	void k_print_str(char * str);
--- kernel/lib/string.asm
+++ kernel/lib/string.asm
@@ -1,5 +1,11 @@
+;; 导出函数
+global memcpy
+global k_print_str
+
+[SECTION .data]
+k_print_pos	dd	160 * 10
+
 [SECTION .text]
-global memcpy					;; 导出函数
 
 ;; memcpy (des_addr, src_addr, size)
 memcpy:
@@ -30,4 +36,42 @@ memcpy.2:
 	pop edi
 	mov esp, ebp
 	pop ebp
+	ret
+
+;; k_print_str ("str")
+k_print_str:
+	push ebp
+	mov ebp, esp
+
+	mov esi, [ebp + 8]			;; 字符串首地址
+	mov edi, [k_print_pos]
+	mov ah, 0xa
+
+k_print_str.1:
+	lodsb
+	test al, al
+	jz k_print_str.2
+	cmp al, 0xa				;; 是否为回车
+	jnz k_print_str.3
+	push eax
+	mov eax, edi
+	mov bl, 160
+	div bl
+	and eax, 0xff
+	inc eax
+	mov bl, 160
+	mul bl
+	mov edi, eax
+	pop eax
+	jmp k_print_str.1
+
+k_print_str.3:
+	mov [gs:edi], ax
+	add edi, 2
+	jmp k_print_str.1
+
+k_print_str.2:
+	mov [k_print_pos], edi
+	pop ebp
+
 	ret
